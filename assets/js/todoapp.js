de482ec2ac795457ef7dc7d9c3f82ececf7c3704
santivivas <santivivas1809@gmail.com>
2024-04-29 10:52:28 -0500

    Created a Delte button for each item

--- assets/js/todoapp.js
+++ assets/js/todoapp.js
@@ -29,7 +29,7 @@ function addTask(){
 }
 
 function renderTask(){
-    const taskList = document.getElementById('taskList');
+    const taskList = document.getElementById("taskList");
     taskList.innerHTML = "";
     tasks.forEach(data =>{
         //Crear el elemento de la lista
@@ -43,8 +43,6 @@ function renderTask(){
         input.id = "check";
         //input.id = "listCheck";
         li.appendChild(input);
-
-        document.delete
     })
 }
 
@@ -57,14 +55,14 @@ function deleteList (){
 }
 
 function addTaskSecond(){
-    const taskInputSecond = document.getElementById('taskInputSecond');
+    const taskInputSecond = document.getElementById("taskInputSecond");
     console.log(taskInputSecond)
-    const taskInputSecondValue = taskInput.value.trim();
+    const taskInputSecondValue = taskInputSecond.value.trim();
 
     if(taskInputSecondValue !== ''){
         tasks.push({
             id: Date.now(),
-            text: taskInputValue,
+            text: taskInputSecondValue,
             completed: false
         })
         renderTaskSecond();
@@ -73,29 +71,38 @@ function addTaskSecond(){
 }
 
 function renderTaskSecond(){
-    const taskListSecond = document.getElementById('taskListSecond');
+    const taskListSecond = document.getElementById("taskListSecond");
     taskListSecond.innerHTML = "";
     tasks.forEach(data =>{
         //Crear el elemento de la lista
         const li = document.createElement("li");
         li.textContent = data.text;
-        li.id = "check";
+        li.id = "check2";
         //li.id = "listItem";
         taskListSecond.appendChild(li);
         const input = document.createElement("input");
         input.type = "checkbox";
-        input.id = "check";
+        input.id = "check2";
         //input.id = "listCheck";
         li.appendChild(input);
-
-        document.delete
+        const deleteButton = document.createElement("button");
+        deleteButton.type = "button";
+        deleteButton.textContent = "Delete";
+        deleteButton.id = "check2"
+        deleteButton.onclick = "deleteWithSingularButton()";
+        li.appendChild(deleteButton);
     })
 }
 
 function deleteListSecond (){
-    const deleteList = document.getElementById("check");
+    const deleteList = document.getElementById("check2");
     //const deleteList = document.getElementById("listItem");
     //const deleteListCheck = document.getElementById("listCheck");
     deleteList.remove();
     //deleteListCheck.remove();
+}
+
+function deleteWithSingularButton (){
+    const deleteSingularButton = document.getElementById("check2");
+    deleteSingularButton.remove();
 }
